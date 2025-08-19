e2bceb3174b6c5366a5e1c70053ca0f63d3ada3d
OuardiHicham <h.ouardi98@gmail.com>
2025-08-19 02:50:26 +0100

    correcrt code

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -6,12 +6,12 @@
 /*   By: houardi <houardi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 04:00:44 by houardi           #+#    #+#             */
-/*   Updated: 2025/08/19 00:46:08 by houardi          ###   ########.fr       */
+/*   Updated: 2025/08/19 02:50:22 by houardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+//correct code 
 int	exit_status(int status)
 {
 	int sig;
@@ -27,7 +27,7 @@ int	exit_status(int status)
 	}
 	return (status);
 }
-
+// yes 
 int	validate_cmd_path(char *path, char *cmd)
 {
 	struct stat st;
